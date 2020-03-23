88b1c0f87f32f1c5fb1f155f9b7f1e14c9ab6109
Danil Nurgaliev <jkonalegi@gmail.com>
2020-03-23 19:15:26 +0300

    Example

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+gem 'graphql'
+
+gem 'rspec'
+gem 'rspec-core'
+gem 'rspec-expectations'
+gem 'pry-byebug'
+gem 'pry-rescue'
+gem 'pry-stack_explorer'
---
+++ app.rb
@@ -0,0 +1,9 @@
+Bundler.require
+
+require 'json'
+require_relative 'graphql_api/types/base_type'
+require_relative 'graphql_api/types/query_type'
+require_relative 'graphql_api/errors'
+require_relative 'graphql_api/error_middleware'
+require_relative 'graphql_api/schema'
+
